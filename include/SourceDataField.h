0cc8d2d3e0b97a73f6207275cb499f25ac170f8c
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2014-03-08 09:44:45 +0100

    removed packet type 1, 2 and 3

--- include/SourceDataField.h
+++ include/SourceDataField.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-                          SourceDataField.h  -  description
+                          SDFRBlock.h  -  description
                              -------------------
     begin                : Thu Nov 29 2001
     copyright            : (C) 2001, 2013 by Andrea Bulgarelli
@@ -14,254 +14,127 @@
  *   For commercial purpose see appropriate license terms                  *
  *                                                                         *
  ***************************************************************************/
-
-#ifndef _SOURCEDATAFIELD_H
-#define _SOURCEDATAFIELD_H
-
-#include "PacketLibDefinition.h"
-#include "PartOfPacket.h"
+ 
+#ifndef _SDFRBLOCK_H
+#define _SDFRBLOCK_H
+#include "SourceDataField.h"
+#include "PacketException.h"
+#include "PacketExceptionFileFormat.h"
+#include "SDFRBBlock.h"
 
 namespace PacketLib
 {
 
-///	\brief A generic source data field.
-class SourceDataField : public PartOfPacket
+///	\brief Class which represents the source data field of Layout 4 (see the Interface Control Document).
+class SDFRBlock : public SourceDataField
 {
 public:
 
-    SourceDataField(const char* sdfName = 0);
+    SDFRBlock(PartOfPacket* pop = 0);
 
-    virtual ~SourceDataField();
+    virtual ~SDFRBlock();
 	
-	/// Sets the stream of byte. This method assigns the value of stream for each field of part of packet
-    virtual bool setByteStream(ByteStreamPtr s, int decodeType) { PartOfPacket::setByteStream(s); return true; };
 
-    /// Gets the total max dimension in bytes of source data field
-    virtual dword getMaxDimension() = 0;
 
-    /// Gets the total max dimension in bytes of source data field for each block
-    /// \param nbloc Number of block
-    virtual dword getMaxDimension(word nblock);
+    virtual bool loadFields(InputText& fp) throw(PacketException*);
 
-    /// Loads the field form .packet file. See PartOfPacket class.
-    virtual bool loadFields(InputText& fp) throw(PacketException*) = 0 ;
-
-    /// \return true if this source data field is compounded of blocks.
-    virtual bool isBlock();
-
-    /// \return true if the dimension of eache block is fixed.
-    virtual bool isFixed();
-
-    /// \return true if the packet is structured as a recursive block
-    virtual bool isRBlock()
-    {
-        return rblock;
-    };
-
-    /// \return true is the number of block is fixed.
-    virtual bool isNumberOfBlockFixed(word rblockIndex = 0);
-
-    /// \return Returns the max number of block for this source data field.
-    virtual word getMaxNumberOfBlock(word rblockIndex = 0);
-
-    /// \return Returns the max number of elements for each block.
-    /// \param nblock Number of block.
-    virtual word getMaxNumberOfElements(word nblock);
-
-    /// Gets the current dimension (in byte) of the source data field.
-    virtual dword getDimension();
+     /// Get a block of number nblock of the group of blocks of the rblock
+    /// with the index rBlockIndex.
+    /// \param nblock the number of the block
+    /// \param rBlockIndex the number of the rblock
+    virtual SDFRBBlock* getBlock(word nblock, word rBlockIndex);
 	
-	virtual dword getDimensionFixedPart() = 0;
+	///Get the fixed part of the source data field
+	virtual ByteStreamPtr getFixedPart() { return block[0].fixed.getByteStream(); };
+	
+	///Get the variable part of the source data field
+	//virtual ByteStreamPtr getVariablePart();
+    
+    /// Returns a pointer of a field in the fixed part of this source data field.
+    /// \param index Represent the index in the list.
+    virtual Field* getFields(word index);
 
-    /// Gets the current dimension (in byte) for a single block.
-    /// \param block Number of block.
-    virtual dword getDimension(word block);
+    
+    /// Returns the value of a field in the fixed part of this source data field.
+    /// \param index Represent the index in the list.
+    virtual word getFieldValue(word index);
 
-    /// Gets the index of field in the data field header that contains the number of block in the
-    /// source data field.
-    virtual word getIndexOfNBlock(word rblockIndex = 0);
-
-    /// Sets the number of real data block for the current packet.
-    virtual void setNumberOfRealDataBlock(word value, word rblockIndex = 0) throw (PacketException*);
-
-    virtual word getSubFromNBlock(word rblockIndex = 0);
-
-    /// \return Returns the number of real data block for the current packet.
-    virtual word getNumberOfRealDataBlock(word rblockIndex = 0);
-
-    /// Gets a field of the source data field.
-    /// \param index The number of field in the source data field.
-    /// \return The field.
-    virtual Field* getFields(word index) = 0;
-
-    /// Gets a field of the source data field.
-    /// \param block The block that contains the field
-    /// \param index The number of field in the source data field (into the block).
-    /// \return The field.
-    virtual Field* getFields(word block, word index);
-
-    /// Gets a field value of the source data field.
-    /// \param index The number of field in the source data field.
-    /// \return The value of field.
-    virtual word getFieldValue(word index) = 0;
-
-    /// Gets a field value of the source data field.
-    /// \param block The block that contains the field.
-    /// \param index The number of field in the source data field (into the block).
-    /// \return The value of field.
-    virtual word getFieldValue(word block, word index);
-
-    /// Gets a field value of the source data field of PTC=5, PFC=1. See comments on PartOfPacket for more details..
-    /// \param block The block that contains the field.
-    /// \param index The number of field in the source data field (into the block).
-    /// \return The value of field.
-    virtual float getFieldValue_5_1(word block, word index);
-
-    virtual float getFieldValue_5_1(word index)
-    {
-        return PartOfPacket::getFieldValue_5_1(index);
-    }
-
-    /// Set a field value of the source data field of PTC=5, PFC=1. See comments on PartOfPacket for more details..
-    /// \param block The block that contains the field.
-    /// \param index The number of field in the source data field (into the block).
-    /// \param value The float
-    virtual void setFieldValue_5_1(word block, word index, float value);
-
-    virtual void setFieldValue_5_1(word index, float value)
-    {
-        PartOfPacket::setFieldValue_5_1(index, value);
-    }
-
-    /// Gets a field value of the source data field of PTC=4, PFC=14. See comments on PartOfPacket for more details..
-    /// \param block The block that contains the field.
-    /// \param index The number of field in the source data field (into the block).
-    /// \return The value of field.
-    virtual signed long getFieldValue_4_14(word block, word index);
-
-    virtual signed long getFieldValue_4_14(word index)
-    {
-        return PartOfPacket::getFieldValue_4_14(index);
-    }
-
-
-    /// Set a field value of the source data field of PTC=4, PFC=14. See comments on PartOfPacket for more details..
-    /// \param block The block that contains the field.
-    /// \param index The number of field in the source data field (into the block).
-    /// \param value The data to be set
-    virtual void setFieldValue_4_14(word block, word index, signed long value);
-
-    virtual void setFieldValue_4_14(word index, signed long value)
-    {
-        PartOfPacket::setFieldValue_4_14(index, value);
-    }
-
-    virtual signed long getFieldValue_4_13(word block, word index);
-
-    virtual signed long getFieldValue_4_13(word index)
-    {
-        return PartOfPacket::getFieldValue_4_13(index);
-    }
-
-    virtual void setFieldValue_4_13(word block, word index, signed long value) throw(PacketException*);
-
-    virtual void setFieldValue_4_13(word index, signed long value) throw(PacketException*)
-    {
-        PartOfPacket::setFieldValue_4_13(index, value);
-    }
-
-    virtual unsigned long getFieldValue_3_14(word block, word index);
-
-    virtual unsigned long getFieldValue_3_14(word index)
-    {
-        return PartOfPacket::getFieldValue_3_14(index);
-    }
-
-    virtual void setFieldValue_3_14(word block, word index, unsigned long value);
-
-    virtual void setFieldValue_3_14(word index, unsigned long value)
-    {
-        PartOfPacket::setFieldValue_3_14(index, value);
-    }
-
-    virtual void setFieldValue_3_13(word block, word index, unsigned long value) throw(PacketException*);
-
-    virtual void setFieldValue_3_13(word index, unsigned long value) throw(PacketException*)
-    {
-        PartOfPacket::setFieldValue_3_13(index, value);
-    }
-
-    virtual unsigned long getFieldValue_3_13(word block, word index);
-
-    virtual unsigned long getFieldValue_3_13(word index)
-    {
-        return PartOfPacket::getFieldValue_3_13(index);
-    }
-
-    /// Sets a field value of the source data field.
-    /// \param index The number of field in the source data field.
+    
+    /// Sets the value of a field in the fixed part of this source data field.
+    /// \param index Represent the index in the list.
     /// \param value The value must be set.
-    virtual void setFieldValue(word index, word value) = 0;
+    virtual void setFieldValue(word index, word value);
 
-    /// Sets a field value of the source data field.
-    /// \param block The block that contains the field.
-    /// \param index The number of field in the source data field (into the block).
-    /// \param value The value must be set.
-    virtual void setFieldValue(word block, word index, word value);
-
-    /// Returns the number of fields for each block.
-    virtual word getNumberOfFields(word block);
-
-    /// Returns the total number of fields for the source data field.
+    
+    /// Returns the number of fields.
     virtual word getNumberOfFields();
 
-    /// Sets the number of real elements for each block.
-    /// \param block Number of block.
-    /// \value value The value must be set.
-    virtual void setNumberOfRealElement(word block, word value);
+    virtual dword getMaxDimension();
 
-    /// Returns the number of real elements for each block.
-    virtual word getNumberOfRealElement(word block);
+    virtual dword getDimension();
+	
+	virtual dword getDimensionFixedPart();
 
-    bool get_reset_output_stream() const;
+	//onlySections = false
+    virtual bool setByteStream(ByteStreamPtr s, int decodeType);
 
-    void set_reset_output_stream(bool value);
+    virtual bool setOutputStream(ByteStreamPtr os, dword first);
 
-    char* printInHexadecimal();
+    virtual ByteStreamPtr generateStream(bool bigendian);
+
+    
+    /// Set the number of blocks (the number of times that a block of a
+    /// determinated type is repeated) for each type of rblock present.
+    /// \param number The number of blocks
+    /// \param rBlockIndex The number of rblock
+    void setNumberOfRealDataBlock(word number, word rblockIndex = 0) throw (PacketException*);
+
+    
+    /// Get the number of blocks (the number of times that a block of a
+    /// determinated type is repeated) for each type of rblock present.
+    /// \param rBlockIndex The number of rblock
+    word getNumberOfRealDataBlock(word rblockIndex = 0);
+
+    
+    /// Prints the value of each field of this part of packet
+    virtual char** printValue(char* addString = "");
+
+    virtual void printValueStdout();
+
+    
+    /// Prints the structure of this part of packet
+    virtual string* printStructure();
+
+    /// return the related RBBlock structure.
+    SDFRBBlock* getBBlock()
+    {
+        return block;
+    }
 
 protected:
-    /// This attribute indicates if the source data field is structured in block
-    bool isblock;
 
-    /// Indicates if the dimension of block is fixed or variable
-    bool fixed;
+    SDFRBBlock* block;
 
-    /// Indicates if the source data field is a recoursive block structure
-    bool rblock;
 
-    /// MANAGEMENT OF BLOCKS
+    
+    /// The number of rblock
+    int numberOfRBlocks;
 
-    /// Represents the type of number of block. If true the number of blocks
-    /// are fixed, if false the number of blocks are variable
-    bool* numberOfBlockFixed;
+    
+    /// for variable block, number of level of headers in which is present the field
+    /// with the number of blocks of the variable part
+    word* headerLevelOfNBlockIndex;
 
-    /// Represents the number of blocks (if numberOfBlockFixed is true) or max
-    /// number of blocks (if numberOfBlockFixed is false) in the structure of
-    /// source data field. This variable not represent the effective number of
-    /// block into the stream of data.
-    word* maxNumberOfBlock;
+    char** rblockFilename;
 
-    /// Real number of blocks.
-    word* numberOfRealDataBlock;
+    bool* rBlockVariable;
 
-    word* indexOfNBlock;
-
-    /// for variable block, valore da sommare per ottenere il numero di eventi (blocchi) reali.
-    word *subFromNBlock;
-
-    bool reset_output_stream;
+    
+    /// The max number of blocks
+    word nblockmax;
 
 };
 
 }
-#endif                           /* SOURCEDATAFIELD_H_INCLUDED_F4BDC5A4 */
+
+#endif /* SDFRBLOCK_H_HEADER_INCLUDED_89D3066E */
--- src/SourceDataField.cpp
+++ src/SourceDataField.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-                          SourceDataField.cpp  -  description
+                          SDFRBlock.cpp  -  description
                              -------------------
     begin                : Thu Nov 29 2001
     copyright            : (C) 2001, 2013 by Andrea Bulgarelli
@@ -15,297 +15,152 @@
  *                                                                         *
  ***************************************************************************/
  
-#include "SourceDataField.h"
+#include "SDFRBlock.h"
 
 using namespace PacketLib;
 
-
-SourceDataField::SourceDataField(const char* sdfName) : PartOfPacket(sdfName)
+SDFRBlock::SDFRBlock(PartOfPacket* pop) : SourceDataField("SDF Recoursive Block")
 {
-    fixed = false;
     isblock = false;
-    rblock = false;
+    fixed = false;
+    rblock = true;
+    block = NULL;
+    previous = pop;
+}
 
-    indexOfNBlock = new word(1);
-    subFromNBlock = new word(1);
-    maxNumberOfBlock = new word(1);
-    numberOfRealDataBlock = new word(1);
-    numberOfBlockFixed = new bool(1);
-    indexOfNBlock[0] = 0;
-    subFromNBlock[0] = 0;
-    maxNumberOfBlock[0] = 0;
-    numberOfRealDataBlock[0] = 0;
-    numberOfBlockFixed[0] = false;
+SDFRBlock::~SDFRBlock()
+{
 
+    delete[] block;
+
+}
+
+bool SDFRBlock::loadFields(InputText& fp) throw(PacketException*)
+{
+    try
+    {
+        numberOfRBlocks = 1;
+        rblockFilename = new char* [numberOfRBlocks];
+        numberOfBlockFixed[0] = true;
+        maxNumberOfBlock[0] = 1;
+        numberOfRealDataBlock[0] = 1;
+        indexOfNBlock[0] = 0;
+        subFromNBlock[0] = 0;
+        rblockFilename[0]= (char*)"Source Data Field";
+        nblockmax = 1;
+        block = (SDFRBBlock*) new SDFRBBlock[nblockmax];
+        block[0].setPreviousPop(previous);
+        block[0].setRBlockType(0);
+        block[0].setID(0);
+        block[0].loadFields(fp);
+        fp.setpos(0);
+        /// It gets the [Identifiers] section
+        fp.getLine("[Identifiers]");
+        return true;
+    }
+    catch(PacketExceptionIO* e)
+    {
+        e->add(" - ");
+        e->add("Configuration file: ");
+        throw e;
+    }
+}
+
+SDFRBBlock* SDFRBlock::getBlock(word nblock,word rBlockIndex)
+{
+    return block[0].getBlock(nblock, rBlockIndex);
+}
+
+/*
+ByteStreamPtr SDFRBlock::getVariablePart() {
+	ByteStreamPtr fixed = getFixedPart();
+	int fixedpartdim = fixed->getDimension();
+	int sdfdim = getDimension();
+    ByteStreamPtr sdfbs = getByteStream();
+	ByteStreamPtr camera = ByteStreamPtr(new ByteStream(sdfbs->stream+fixedpartdim, sdfdim-fixedpartdim, sdfbs->isBigendian()));
+	return camera;
+}
+*/
+
+dword SDFRBlock::getMaxDimension()
+{
+    return block[0].getMaxDimension();
+}
+
+dword SDFRBlock::getDimension()
+{
+    return block[0].getDimension();
+}
+
+dword SDFRBlock::getDimensionFixedPart()
+{
+    return block[0].fixed.getDimension();
+}
+
+void SDFRBlock::setNumberOfRealDataBlock(word number, word rblockIndex)  throw (PacketException*)
+{
+    /// The block[0] is the only block present
+    block[0].setNumberOfRealDataBlock(number, rblockIndex);
     reset_output_stream = true;
 }
 
-SourceDataField::~SourceDataField()
-{
-    delete indexOfNBlock;
-    delete subFromNBlock;
-    delete maxNumberOfBlock;
-    delete numberOfRealDataBlock;
-    delete numberOfBlockFixed;
-}
 
-char* SourceDataField::printInHexadecimal()
+word SDFRBlock::getNumberOfRealDataBlock(word rblockIndex)
 {
-    dword dim = getDimension();
-    ByteStream b(stream->stream, dim, true);
-    char* c = b.printStreamInHexadecimal();
-    return c;
+    /// The block[0] is the only block present
+    return  block[0].getNumberOfRealDataBlock(rblockIndex);
 }
 
 
-bool SourceDataField::isBlock()
+bool SDFRBlock::setOutputStream(ByteStreamPtr os, dword first)
 {
-    return isblock;
+    return block[0].setOutputStream(os, first);
+}
+
+ByteStreamPtr SDFRBlock::generateStream(bool bigendian)
+{
+    return block[0].generateStream(bigendian);
+}
+
+bool SDFRBlock::setByteStream(ByteStreamPtr s, int decodeType)
+{
+    stream = s;
+    return block[0].setByteStream(s, decodeType);
+}
+
+Field* SDFRBlock::getFields(word index)
+{
+    return block[0].getFields(index);
+}
+
+word SDFRBlock::getFieldValue(word index)
+{
+    return block[0].getFieldValue(index);
+}
+
+void SDFRBlock::setFieldValue(word index, word value)
+{
+    return block[0].setFieldValue(index, value);
+}
+
+word SDFRBlock::getNumberOfFields()
+{
+    return block[0].getNumberOfFields();
+}
+
+char** SDFRBlock::printValue(char* addString)
+{
+    return block[0].printValue(addString);
+}
+
+void SDFRBlock::printValueStdout()
+{
+    block[0].printValueStdout();
+}
+
+string* SDFRBlock::printStructure()
+{
+    return block[0].printStructure();
 }
 
 
-
-bool SourceDataField::isFixed()
-{
-    return fixed;
-}
-
-
-
-bool SourceDataField::isNumberOfBlockFixed(word rblockIndex)
-{
-    return numberOfBlockFixed[rblockIndex];
-}
-
-
-word SourceDataField::getMaxNumberOfBlock(word rblockIndex)
-{
-    return maxNumberOfBlock[rblockIndex];
-}
-
-
-//##ModelId=3C9AEC0B006F
-word SourceDataField::getSubFromNBlock(word rblockIndex)
-{
-    return subFromNBlock[rblockIndex];
-}
-
-
-word SourceDataField::getIndexOfNBlock(word rblockIndex)
-{
-    return indexOfNBlock[rblockIndex];
-}
-
-
-
-void SourceDataField::setNumberOfRealDataBlock(word number, word rblockIndex) throw (PacketException*)
-{
-    numberOfRealDataBlock[rblockIndex] = number;
-    reset_output_stream = true;
-}
-
-
-
-word SourceDataField::getNumberOfRealDataBlock(word rblockIndex)
-{
-    return  numberOfRealDataBlock[rblockIndex];
-}
-
-
-
-word SourceDataField::getNumberOfFields()
-{
-    return PartOfPacket::getNumberOfFields();
-};
-
-
-word SourceDataField::getNumberOfFields(word block)
-{
-    return 0;
-};
-
-//##ModelId=3DA3E60F0226
-dword SourceDataField::getDimension()
-{
-    return PartOfPacket::getDimension();
-}
-
-
-
-word SourceDataField::getNumberOfRealElement(word block)
-{
-    return 0;
-}
-
-
-void SourceDataField::setNumberOfRealElement(word block, word value)
-{
-
-}
-
-void SourceDataField::setFieldValue(word block, word index, word value)
-{
-
-}
-
-Field* SourceDataField::getFields(word block, word index)
-{
-    return 0;
-}
-
-word SourceDataField::getFieldValue(word block, word index)
-{
-    return 0;
-}
-
-float SourceDataField::getFieldValue_5_1(word block, word index)
-{
-    union u_tag
-    {
-    	/// 32 bit
-        unsigned long i;
-        /// 32 bit single precision
-        float f;	
-    } u;
-    u.i = (getFieldValue(block, index) << 16) | getFieldValue(block, index + 1);
-    return u.f;
-}
-
-void SourceDataField::setFieldValue_5_1(word block, word index, float value)
-{
-    union u_tag
-    {
-    	/// 32 bit
-    	unsigned long i;		
-        /// 32 bit single precision
-        float f;	
-    } u;
-    word w;
-    u.f = value;
-    w = (word)(u.i >> 16);
-    setFieldValue(block, index, w);
-    w = (word)(0xFFFF & u.i);
-    setFieldValue(block, index + 1, w);
-}
-
-long SourceDataField::getFieldValue_4_14(word block, word index)
-{
-    long l;
-    l = (long)(getFieldValue(block, index) << 16) | (long)getFieldValue(block, index + 1);
-    return l;
-}
-
-void SourceDataField::setFieldValue_4_14(word block, word index, signed long value)
-{
-    word w;
-    w = (word)(value >> 16);
-    setFieldValue(block, index, w);
-    w = (word) (0xFFFF & value);
-    setFieldValue(block, index + 1, w);
-}
-
-signed long SourceDataField::getFieldValue_4_13(word block, word index)
-{
-    union u_tag
-    {
-    	/// 32 bit
-    	unsigned long u;		
-        signed long s;
-    } us;
-    us.u = getFieldValue_3_14(block, index);
-    /// gets the sign
-    unsigned long sign = (us.u  >> 23); 
-    unsigned long wh = us.u & 0x007FFFFF;
-    /// gets a long 32 bit
-    if(sign == 1)
-        us.u = 0x7F800000 + wh + (sign << 31);
-    else
-        us.u = wh + (sign << 31);
-    return us.s;
-}
-
-void  SourceDataField::setFieldValue_4_13(word block, word index, signed long value) throw(PacketException*)
-{
-    union u_tag
-    {
-    	/// 32 bit
-        unsigned long u;		
-        signed long s;
-    } us;
-    if(value > U24BITINTGEGERSIGNED_MAX)
-        throw new PacketException("setFieldValue_4_13(): the max value of 24 bit signed integer should be 8388607");
-    if(value < U24BITINTGEGERSIGNED_MIN)
-        throw new PacketException("setFieldValue_4_13(): the min value of 24 bit signed integer should be -8388607");
-    us.s = value;
-    unsigned long sign = (us.u >> 31);
-    /// 23 bit
-    unsigned long wh = us.u & 0x007FFFFF; 
-    unsigned long value2 = 0;
-    value2 = wh + (sign << 23);
-    setFieldValue_3_14(block, index, value2);
-}
-
-unsigned long SourceDataField::getFieldValue_3_14(word block, word index)
-{
-    dword l;
-    l = (dword)(getFieldValue(block, index) << 16) | (dword)getFieldValue(block, index + 1);
-    return l;
-}
-
-void SourceDataField::setFieldValue_3_14(word block, word index, unsigned long value)
-{
-    word w;
-    w = (word)(value >> 16);
-    setFieldValue(block, index, w);
-    w = (word) (0xFFFF & value);
-    setFieldValue(block, index + 1, w);
-}
-
-unsigned long SourceDataField::getFieldValue_3_13(word block, word index)
-{
-    word wh, wl;
-    wh = getFieldValue(block, index);
-    wl = getFieldValue(block, index + 1);
-    return (dword)(wh << 8) | (dword)(wl & 0xFF);
-}
-
-void SourceDataField::setFieldValue_3_13(word block, word index, unsigned long value) throw(PacketException*)
-{
-    word w;
-    if(value > U24BITINTGEGERUNSIGNED_MAX)
-        throw new PacketException("setFieldValue_3_13(): the max value of 24 bit unsigned integer should be 16777215");
-    w = (word)(value >> 8);
-    setFieldValue(block, index, w);
-    w = (word) (0xFF & value);
-    setFieldValue(block, index + 1, w);
-}
-
-
-dword SourceDataField::getDimension(word block)
-{
-    return 0;
-}
-
-dword SourceDataField::getMaxDimension(word nblock)
-{
-    return 0;
-}
-
-
-word SourceDataField::getMaxNumberOfElements(word nblock)
-{
-    return 0;
-}
-
-
-bool SourceDataField::get_reset_output_stream() const
-{
-    return reset_output_stream;
-}
-
-
-
-void SourceDataField::set_reset_output_stream(bool value)
-{
-    reset_output_stream = value;
-}
