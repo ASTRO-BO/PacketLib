3c7301595a8375ee7433cbbcb9ab09ee5073cab7
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2015-02-19 09:00:38 +0100

    xml example

--- examples/xml/packetlib_example.cpp
+++ examples/xml/packetlib_example.cpp
@@ -38,12 +38,13 @@ int main(int argc, char* argv[])
 
 		PacketLib::Field* apid = packetType->getPacketHeader()->getField("APID");
 		std::cout << apid->getType() << std::endl;
-		assert(apid->getType() == PacketLib::LogicalFieldDataType::UINT11);
-		assert(apid->getName().compare("APID") == 0);
+		//assert(apid->getType() == PacketLib::LogicalFieldDataType::UINT11);
+		//assert(apid->getName().compare("APID") == 0);
 	
 		PacketLib::Field* arrayID = packetType->getPacketDataFieldHeader()->getField("ArrayID");
-		assert(arrayID->getType() == PacketLib::LogicalFieldDataType::UINT16);
-		assert(arrayID->getName().compare("ArrayID") == 0);
+		//assert(arrayID->getType() == PacketLib::LogicalFieldDataType::UINT16);
+		//assert(arrayID->getName().compare("ArrayID") == 0);
+		
 	}
 	catch(PacketLib::PacketException* e) {
 		std::cerr << "Bad config file: " << e->geterror() << std::endl;
@@ -57,6 +58,10 @@ int main(int argc, char* argv[])
 	for(unsigned int i=0; i<10; i++) {
 		PacketLib::ByteStreamPtr rawPacket = rawPackets.getNext();
 		PacketLib::Packet* packet = ps.getPacket(rawPacket);
+		
+		//PacketLib::Field* blockfield = packet->getPacketSourceDataField()->getBlock(0)->getField("FADC00");
+		//assert(blockfield->getType() == PacketLib::LogicalFieldDataType::UINT16);
+
 
 		// use reflection interface to read content
 		uint16_t apid = packet->getPacketHeader()->getFieldValue("APID");
@@ -68,10 +73,16 @@ int main(int argc, char* argv[])
 		
 
 		//get each single pixel as a 1-dimensional array
-		//for(unsigned int j=0; j<npixels; j++) {
-		//	PacketLib::byte* buff = packet->getPacketSourceDataField()->getBlock(j)->getByteStream()->getStream();
-		//}
+		for(unsigned int j=0; j<npixels; j++) {
+			
+			uint16_t* pix = (uint16_t*) packet->getPacketSourceDataField()->getBlock(j)->getByteStream()->getStream();
+			for(uint16_t sample=0; sample<nsamples; sample++)
+				cout << pix[sample] << " ";
+			cout << endl;
+			
+		}
 		
+		//access to each single sample
 		for(uint16_t pixel=0; pixel<npixels; pixel++) {
 			for(uint16_t sample=0; sample<nsamples; sample++) {
 				cout << packet->getPacketSourceDataField()->getBlock(pixel)->getFieldValue(sample) << " ";
